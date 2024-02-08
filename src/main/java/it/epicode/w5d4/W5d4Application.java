6581ff8258f54c38c97fedd3e52693551c70ee9f
zAeryos <dave_13_08@hotmail.it>
2024-02-08 16:04:37 +0100

    Initial commit

---
+++ src/main/java/it/epicode/w5d4/W5d4Application.java
@@ -0,0 +1,13 @@
+package it.epicode.w5d4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W5d4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W5d4Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/w5d4/W5d4ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.w5d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W5d4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
